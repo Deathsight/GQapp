b479246800df01f48a58cf1b0dea3992207220f8
Deathsight <57475781+Deathsight@users.noreply.github.com>
2021-01-10 15:13:31 +0300

    connection test

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
+import db from "./db"
+
 export default function App() {
+  useEffect(()=> {
+    fetch()
+  },[])
+  
+  const fetch = async () => {
+      const response = await db.collection("categories").doc("car").get()
+      console.log("response", response.data())
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+ 
       <StatusBar style="auto" />
     </View>
   );
